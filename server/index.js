050a7bbc9867b757e970373765164927911b2ac5
Marlon Torres <marlon.dt@hotmail.com>
2026-02-04 11:13:26 +0100

    feat(server): add Express mock server for user API endpoints

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+
+let users = [
+    { name: 'Marlon', lastName: 'Torres', dateOfBirth: '1995-12-24'},
+    { name: 'Maybelline', lastName: 'Mejía', dateOfBirth: '1996-02-05'},
+    { name: 'Norma', lastName: 'Torres', dateOfBirth: '1975-06-07'},
+    { name: 'Oscar', lastName: 'Guerrero', dateOfBirth: '1997-06-30'},
+    { name: 'Danna', lastName: 'Marroquín', dateOfBirth: '2023-12-28'},
+]
+
+app.get('/api/users/1/users', (req, res)=> {
+    res.json(users)
+})
+
+app.post('/api/users/1/users', (req, post) => {
+    const newUser = req.body 
+    users.push(newUser)
+    res.status(204).send()
+})
+
+app.listen(PORT, () => {
+    console.log(`My server is running on port ${PORT}`)
+})
