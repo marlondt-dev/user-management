859615e78049fde2efda6ec83aee68dc04c53221
Marlon Torres <marlon.dt@hotmail.com>
2026-02-06 01:56:11 +0100

    feat(app.vue): added nav with options and change language options

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,71 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import LanguageSelector from '@/components/LanguageSelector.vue';
+import { useI18n } from 'vue-i18n';
+
+const {t} = useI18n()
+
 </script>
 
 <template>
   <div id="app">
-    <h1>Administración de usuarios</h1>    
+    <nav>
+      <div class="nav-links">
+      <RouterLink to="/">{{ t('nav.users') }}</RouterLink>
+      <RouterLink to="/create">{{ t('nav.createUsers') }}</RouterLink>
+      </div>
+    
+    <LanguageSelector />
+    </nav>   
     <RouterView />
   </div>
   
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+nav {
+  background-color: $white;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+  @include tablet {
+    display: flex;
+    flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  }
+  
+  padding: 1rem 2rem;
+
+  .nav-links {
+    display: flex;
+    gap: 2rem;
+
+    a {
+      color: $text-secondary;
+      font-weight: 500;
+      padding: 0.5rem 0;
+      transition: color 0.2s;
+
+      &:hover {
+        color: $primary;
+      }
+
+      &.router-link-active {
+        color: $primary;
+        border-bottom: 2px solid $primary;
+      }
+    }
+  }
+}
+
+
+main {
+  max-width: 75rem;
+  margin: 2rem auto;
+  padding: 0 2rem;
+}
 </style>
+
