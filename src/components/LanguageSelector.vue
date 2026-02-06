d7a5539e2cfd1400fde87399c6362e4d23beee64
Marlon Torres <marlon.dt@hotmail.com>
2026-02-06 01:56:38 +0100

    feat(components): created language selector component

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n()
+
+const languages = [
+    { code: 'es', name: 'Español', flag: 'ES'},
+    { code: 'en', name: 'English', flag: 'EN'},
+]
+
+const changeLanguage = (code: string) => {
+    locale.value = code
+}
+
+</script>
+
+<template>
+    <div class="language-selector">
+        <button v-for="lang in languages" :key="lang.code" :class="{ active: locale === lang.code }" 
+        @click="changeLanguage(lang.code)" :title="lang.name">{{ lang.flag }}</button>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+    .language-selector {
+        margin-left: auto;
+        display: flex;
+        gap: 0.5rem;
+
+        button{
+            @include button($dark-gray);
+            &.active {
+            background: $primary;
+            color: $white;
+            border-color: $primary;
+        }
+        }  
+        }
+
+</style>
