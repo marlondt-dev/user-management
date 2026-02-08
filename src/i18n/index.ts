4c202b20118923219d878541924632c8b5e36e79
Marlon Torres <marlon.dt@hotmail.com>
2026-02-08 04:37:47 +0100

    feat(in18): create the missing languates for translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,9 @@
 import { createI18n } from "vue-i18n";
 import es from "@/i18n/es.json"
 import en from "@/i18n/en.json"
+import it from "@/i18n/it.json"
+import fr from "@/i18n/fr.json"
+import de from "@/i18n/de.json"
 
 const i18n = createI18n({
     legacy: false,
@@ -8,7 +11,10 @@ const i18n = createI18n({
     fallbackLocale: 'en',
     messages: {
         es,
-        en
+        en,
+        it,
+        fr,
+        de
     }
 })
 
